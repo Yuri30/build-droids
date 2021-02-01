c2b1fb958f5b8f25869fb23d516c71d8c74e1c37
Yuri30 <yu.johnathan10@gmail.com>
2021-02-01 19:04:24 -0300

    removendo pastas desnecessarias

--- builddroids/settings.py
+++ builddroids/settings.py
@@ -137,8 +137,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_URL = '/apps/demanda/svg/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "svg")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
